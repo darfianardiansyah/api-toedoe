293db76afa6c345bc09a5f215cd92c26f3e6983f
darfianardiansyah <darfianardiansyah@email.com>
2025-07-10 11:07:41 +0700

    add: Delete a Resource

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -56,6 +56,8 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
