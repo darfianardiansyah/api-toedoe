28e73d58b0be880382d62d97156269d4898e935d
darfianardiansyah <darfianardiansyah@gmail.com>
2025-07-11 15:00:11 +0700

    add: Create login endpoint with Token response

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\V1\CompleteTaskController;
-use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\V1\CompleteTaskController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,4 +13,8 @@ Route::get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [LoginController::class]);
 });
