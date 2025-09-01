bcfc91deeaf78fa973a9143f9dccaa154154b1a7
darfianardiansyah <darfianardiansyah@gmail.com>
2025-09-01 14:00:56 +0700

    Adding Summaries endpoint

---
+++ app/Http/Controllers/Api/V2/SummaryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api\V2;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskSummaryResource;
+
+class SummaryController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $tasks = $request->user()->tasksSummary();
+
+        return $tasks->mapToGroups(function ($item, $key) {
+            return [
+                ($item->is_completed ? 'completed' : 'uncompleted') => TaskSummaryResource::make($item)
+            ];
+        });
+    }
+}
